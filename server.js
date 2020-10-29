59e972bfc36e6e0a6ac87497ef609a19dc924163
berhane beyene <monoxica2004@yahoo.com>
2020-10-29 15:30:25 -0500

    server.js initial commit

---
+++ server.js
@@ -0,0 +1,20 @@
+const express = require('express');
+const logger = require('morgan');
+const mongoose = require('mongoose');
+
+var app = express();
+
+var PORT = (process.env.PORT || 5000);
+
+
+app.use(logger("dev"));
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+app.use(express.static("public"));
+
+mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/workout", { useNewUrlParser: true });
+
+
+app.listen(PORT, () => {
+  console.log(`App running on port ${PORT}!`);
+});
