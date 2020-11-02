7ee24b89efbf49ade33c60c640f3117f01bba17a
berhane beyene <monoxica2004@yahoo.com>
2020-11-02 16:08:29 -0600

    deploying

--- server.js
+++ server.js
@@ -3,9 +3,7 @@ const logger = require('morgan');
 const mongoose = require('mongoose');
 
 var app = express();
-
 var PORT = (process.env.PORT || 5000);
-
 
 app.use(logger("dev"));
 app.use(express.urlencoded({ extended: true }));
@@ -13,8 +11,14 @@ app.use(express.json());
 app.use(express.static("public"));
 
 const db = require("./models");
-mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/workout", { useNewUrlParser: true });
-
+mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/workout',
+  {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useCreateIndex: true,
+    useFindAndModify: false
+  }
+);
 
 require("./routes/apiRoutes")(app);
 require("./routes/htmlRoutes")(app);
